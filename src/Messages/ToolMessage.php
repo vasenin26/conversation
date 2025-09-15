a86cc33c8d22cafcc6f15b97512bcdaa63c3006e
vasenin26 <leonid.vasenin@gmail.com>
2025-09-15 19:46:57 +0300

    save tool call args

--- src/Messages/ToolMessage.php
+++ src/Messages/ToolMessage.php
@@ -9,8 +9,10 @@ readonly class ToolMessage implements Message
     const TYPE = 'tool';
 
     public function __construct(
+        public bool   $success,
         public string $id,
         public string $name,
+        public array  $args,
         public string $result,
     )
     {
@@ -19,8 +21,10 @@ readonly class ToolMessage implements Message
     public function getContent(): array
     {
         return [
+            'success' => $this->success,
             'id' => $this->id,
             'name' => $this->name,
+            'args' => $this->args,
             'result' => $this->result
         ];
     }
@@ -29,12 +33,14 @@ readonly class ToolMessage implements Message
     {
         return self::TYPE;
     }
-    
+
     public static function createFromData(array $content): self
     {
         return new self(
+            $content['success'],
             $content['id'],
             $content['name'],
+            $content['args'],
             $content['result']
         );
     }
