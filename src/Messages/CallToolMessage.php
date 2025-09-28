0f50de02d3555f52e9500aa580843fb1c1d87f3d
vasenin26 <leonid.vasenin@gmail.com>
2025-09-28 20:29:43 +0300

    add manual call tool message

---
+++ src/Messages/CallToolMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vasenin26\Conversation\Messages;
+
+use Vasenin26\Conversation\Message;
+
+readonly class CallToolMessage implements Message
+{
+    const TYPE = 'call-tool';
+
+    public function __construct(
+        public string $name,
+        public array $args,
+    )
+    {
+    }
+
+    public function getContent(): array
+    {
+        return [
+            'name' => $this->name,
+            'args' => $this->args,
+        ];
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public static function createFromData(array $content): self
+    {
+        return new self(
+            $content['name'],
+            $content['args'],
+        );
+    }
+}
